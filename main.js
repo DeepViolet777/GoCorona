1f91e48c03fc49d47d1dbbfc8372d79cd1782bd0
Malorodova <LAbsente@yandex.ru>
2022-02-22 22:32:30 +0300

    Inicial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-icon-wrapper').onclick = function(){
+    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+    document.querySelector('.menu_body').classList.toggle('open');
+  }
+
